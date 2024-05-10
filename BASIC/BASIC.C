6ebaf509cf07c7308c278e189f56cd1c17eeaaae
AlexZmasser <63077643+AlexZmasser@users.noreply.github.com>
2024-05-10 15:38:58 +1000

    Updates

--- BASIC/BASIC.C
+++ BASIC/BASIC.C
@@ -1,21 +1,48 @@
 #include <stdio.h>
 #include <string.h>
 
-int main ()
+int main()
 {
 
-    char DUNGEON [6][6]={{'#','#','#','#','#','#'},
-                         {'#',' ',' ',' ','D','#'},
-                         {'#',' ','K',' ',' ','#'},
-                         {'#',' ',' ',' ',' ','#'},
-                         {'#',' ',' ','S',' ','#'},
-                         {'#','#','#','#','#','#'},};
+    char DUNGEON[6][6] = {
+        {'#', '#', '#', '#', '#', '#'},
+        {'#', ' ', ' ', ' ', 'D', '#'},
+        {'#', ' ', 'K', ' ', ' ', '#'},
+        {'#', ' ', ' ', ' ', ' ', '#'},
+        {'#', ' ', ' ', 'S', ' ', '#'},
+        {'#', '#', '#', '#', '#', '#'},
+    };
     bool Key = false;
     bool Door = false;
+    int Player[6][6];
+    char cont = 'y';
+    char move;
 
-    for (int i = 0; i < 6; i++)
+    while (cont != 'n')
     {
-        printf ("%d", DUNGEON[i][0]);  
+
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                
+                if (DUNGEON[i][j] == 'S')
+                {
+                    printf("P");
+                    Player[i][j];
+                }
+                printf("%c ", DUNGEON[i][j]);
+            }
+            printf("\n");
+        }
+        printf("Where would you like to move to n,w,s,e ?");
+        scanf("%c", &move); 
+        
+        
+
+
+        printf("Would you like to continue y/n? \n");
+        scanf("%c", &cont);
     }
 
     return 0;
